58f8724b21ef6e1a77e98d3384a9a965ceb9809e
RickyJian <rjricky03bc@gmail.com>
2019-01-26 01:08:47 +0800

    add web config

---
+++ src/main/java/idv/rickyjian/config/WebConfig.java
@@ -0,0 +1,60 @@
+package idv.rickyjian.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+
+@Configuration
+public class WebConfig {
+
+	/**
+	 * 
+	 * 模板設定
+	 * setPrefix：設定前綴字
+	 * setSuffix：外部訪問路徑
+	 * 
+	 */
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setPrefix("classpath:/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setCharacterEncoding("UTF-8");
+		templateResolver.setCacheable(true);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setOrder(0);
+		return templateResolver;
+	}
+
+
+	/**
+	 * 
+	 * 模板設定
+	 * 
+	 */
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		return templateEngine;
+	}
+
+	/**
+	 * 
+	 * Thymeleaf 模板設定
+	 * 
+	 */
+	@Bean 
+	public ThymeleafViewResolver thymeleafViewResolver() {
+		ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+		thymeleafViewResolver.setCharacterEncoding("UTF-8");
+		thymeleafViewResolver.setTemplateEngine(templateEngine());
+		return thymeleafViewResolver;
+
+	}
+
+}
