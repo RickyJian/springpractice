d9f502f1b7d2fdcaf6a0ce005aaf5a7c8990d4d0
RickyJian <rjricky03bc@gmail.com>
2019-01-26 15:17:31 +0800

    complete controller

--- src/main/java/idv/rickyjian/controller/RouteController.java
+++ src/main/java/idv/rickyjian/controller/RouteController.java
@@ -1,9 +1,15 @@
 package idv.rickyjian.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import idv.rickyjian.bean.UserBean;
 
@@ -19,9 +25,9 @@ public class RouteController {
 	}
 	
 //	設定 index URL
-	@RequestMapping("/")
-	public ModelAndView index() {
-		return new ModelAndView("index");
+	@RequestMapping("/{userName}")
+	public ModelAndView index(@PathVariable("userName") String userName) {
+		return new ModelAndView("index","userName",userName);
 	}
 	
 	
@@ -46,6 +52,30 @@ public class RouteController {
 		mav.addObject("userName",userbean.getUserName());
 //		導頁至 login.html
 		return mav;
+	}
+	
+//	login 處理
+//	AJAX + 封裝成 bean
+	@RequestMapping("/dologinwithform")
+	public @ResponseBody String dologin(UserBean userbean) throws JsonProcessingException {
+//		回傳資料轉成 JSON 格式
+		ObjectMapper objectMapper = new ObjectMapper();
+		ObjectNode objectNode= objectMapper.createObjectNode();
+		objectNode.put("success",true);
+		objectNode.put("userName",userbean.getUserName());
+		return objectMapper.writeValueAsString(objectNode);
+	}
+	
+//	login 處理
+//	AJAX + 指定參數
+	@RequestMapping("/dologin")
+	public @ResponseBody String dologin(@RequestParam String userName , String password) throws JsonProcessingException {
+//		回傳資料轉成 JSON 格式
+		ObjectMapper objectMapper = new ObjectMapper();
+		ObjectNode objectNode= objectMapper.createObjectNode();
+		objectNode.put("success",true);
+		objectNode.put("userName",userName);
+		return objectMapper.writeValueAsString(objectNode);
 	}
 	
 }
