c82e7afd4a7d2e73dda2335b2638ec1486f2dc8b
RickyJian <rjricky03bc@gmail.com>
2019-01-26 17:53:46 +0800

    add login logic

--- src/main/java/idv/rickyjian/service/impl/UserServiceImpl.java
+++ src/main/java/idv/rickyjian/service/impl/UserServiceImpl.java
@@ -7,9 +7,12 @@ import idv.rickyjian.service.UserService;
 @Service
 public class UserServiceImpl implements UserService {
 
+//	登入驗證 商業邏輯
 	@Override
 	public boolean isAccountValid(String userName, String password) {
-		// TODO Auto-generated method stub
+		if (userName.equals("ricky")&&password.equals("0123")) {
+			return true;
+		}
 		return false;
 	}
 
