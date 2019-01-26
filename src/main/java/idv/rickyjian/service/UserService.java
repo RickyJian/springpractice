6a614223a29b075b2fea6b0025ea34dbc2b76bf5
RickyJian <rjricky03bc@gmail.com>
2019-01-26 17:13:51 +0800

    init service

---
+++ src/main/java/idv/rickyjian/service/UserService.java
@@ -0,0 +1,7 @@
+package idv.rickyjian.service;
+
+public interface UserService {
+
+	public boolean isAccountValid(String userName ,String password);
+	
+}
---
+++ src/main/java/idv/rickyjian/service/impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package idv.rickyjian.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import idv.rickyjian.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public boolean isAccountValid(String userName, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
